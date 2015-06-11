d725dbb3176e708188ca2816b65be26d172002c6
DarioGT <unicorniod64@gmail.com>
2015-06-11 12:21:22 -0400

    Eliminar modelos de prueba jsofield

--- blog/models.py
+++ blog/models.py
@@ -62,31 +62,3 @@ class JsonModel(models.Model):
         return self.code
 
 
-
-class JSONFieldTestModel(models.Model):
-    json = JSONField("test", null=True, blank=True)
-
-    class Meta:
-        app_label = 'jsonfield2'
-
-
-class JSONFieldWithDefaultTestModel(models.Model):
-    json = JSONField(default={"sukasuka": "YAAAAAZ"})
-
-    class Meta:
-        app_label = 'jsonfield2'
-
-
-class BlankJSONFieldTestModel(models.Model):
-    null_json = JSONField(null=True)
-    blank_json = JSONField(blank=True)
-
-    class Meta:
-        app_label = 'jsonfield2'
-
-
-class CallableDefaultModel(models.Model):
-    json = JSONField(default=lambda: {'x': 2})
-
-    class Meta:
-        app_label = 'jsonfield2'
